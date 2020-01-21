69942043dfeebc52fde7edfc3f0564a349bcb64a
Karol Czulkowski <karol.czulkowski@gmail.com>
2020-01-21 21:06:07 +0100

    Fix compilation errors.

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,17 @@
-{-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE UndecidableInstances       #-}
+{-# LANGUAGE StandaloneDeriving         #-}
 {-# LANGUAGE EmptyDataDecls             #-}
 {-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GADTs                      #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes       #-}
-{-# LANGUAGE TemplateHaskell   #-}
-{-# LANGUAGE TypeFamilies      #-}
-{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE DeriveGeneric              #-}
 
 module Main where
 
@@ -18,39 +19,27 @@ import GHC.Generics
 import Data.Text
 import Data.Aeson
 import Yesod
+import Database.Persist
+import Database.Persist.Postgresql
+import Database.Persist.TH
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
-EmailMessage
+EmailMessage json
     title String
     body String
     deriving Show
 |]
-instance Generic EmailMessage
 
 data HelloWorld = HelloWorld
 
---data EmailMessage = EmailMessage {
---    title :: Text,
---    body  :: Text
---  } deriving (Generic, Show, Eq)
-
-instance ToJSON EmailMessage
-instance FromJSON EmailMessage
-
 mkYesod "HelloWorld" [parseRoutes|
-/ HomeR GET
 /sendmail SendMailR POST
 |]
 
 instance Yesod HelloWorld
 
-getHomeR :: Handler Text
-getHomeR = return "Hello, World!"
-
 postSendMailR :: Handler Text
-postSendMailR = do
- msg <- requireCheckJsonBody :: Handler EmailMessage
- return $ pack . show $ msg
+postSendMailR = (pack . show) <$> (requireCheckJsonBody :: Handler EmailMessage)
 
 main :: IO ()
 main = warp 3000 HelloWorld
